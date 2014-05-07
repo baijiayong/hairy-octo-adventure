2fb935f1093d459954d5a8e2ac68a71c58174072
shihang <study1228.sh@gmail.com>
2014-05-07 13:54:45 +0800

    Add CalcTest

---
+++ CalcTest.java
@@ -0,0 +1,14 @@
+public class CalcTest extends BearUnitTest implements BearUnitTestCase
+{  
+    public void test() throws Exception
+    {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        assertEquals("Fizz", fizzBuzz.say(3));
+        assertEquals("Buzz", fizzBuzz.say(5));
+        assertEquals("FizzBuzz", fizzBuzz.say(15));
+        assertEquals("13", fizzBuzz.say(13));
+        assertEquals("20", fizzBuzz.say(20));
+    }      
+}
+
