9da17739bdf9444550fd60fd55205e6faccad402
shihang <study1228.sh@gmail.com>
2014-05-07 10:16:15 +0800

    添加Fizz.java

---
+++ Fizz.java
@@ -0,0 +1,25 @@
+public class Fizz
+{
+    public static void main(String[] args)
+    {        
+        for(int i = 1; i < 101; i++)
+        {
+            if(i % 3 == 0 && i % 5 == 0)
+            {
+                System.out.println( i + ":FizzBuzz");
+            }
+            else if(i % 3 == 0 && i % 5 != 0)
+            {
+                System.out.println(i + ":Fizz");
+            }
+            else if(i % 5 == 0 && i % 3 != 0)
+            {
+                System.out.println(i + ":Buzz");
+            }
+            else
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
