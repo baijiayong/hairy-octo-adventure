be3891c82f81f64b700a82216323e3e6f6ef1f6b
shihang <study1228.sh@gmail.com>
2014-05-07 13:09:03 +0800

    将Judgment抽取成类并使用

--- Fizz.java
+++ Fizz.java
@@ -1,42 +1,17 @@
 public class Fizz
-{
-    
-    public static String judgment(int i)
-    {
-            if(i % 3 == 0 && i % 5 == 0)
-            {
-                return "FizzBuzz";
-            }
-            else if(i % 3 == 0 && i % 5 != 0)
-            {
-                return "Fizz";
-            }
-            else if(i % 5 == 0 && i % 3 != 0)
-            {
-                return "Buzz";
-            }
-            else
-            {
-                return String.valueOf(i);
-            }
-
-    }
-    
-    
-    
-    
-    
+{  
     public static void main(String[] args)
     {
         Test fizzTest = new Test();
+        Judgment judge = new Judgment();
         try
         {
             
-            fizzTest.test("Fizz", judgment(3));
-            fizzTest.test("Buzz", judgment(5));
-            fizzTest.test("FizzBuzz", judgment(15));
-            fizzTest.test("13", judgment(13));
-            fizzTest.test("20", judgment(20));
+            fizzTest.test("Fizz", judge.judgment(3));
+            fizzTest.test("Buzz", judge.judgment(5));
+            fizzTest.test("FizzBuzz", judge.judgment(15));
+            fizzTest.test("13", judge.judgment(13));
+            fizzTest.test("20", judge.judgment(20));
         }
         catch(Exception e)
         {
---
+++ Judgment.java
@@ -0,0 +1,22 @@
+class Judgment
+{
+     public static String judgment(int i)
+    {
+        if(i % 3 == 0 && i % 5 == 0)
+        {
+            return "FizzBuzz";
+        }
+        else if(i % 3 == 0 && i % 5 != 0)
+        {
+            return "Fizz";
+        }
+        else if(i % 5 == 0 && i % 3 != 0)
+        {
+            return "Buzz";
+        }
+        else
+        {
+            return String.valueOf(i);
+        }
+    }    
+}
