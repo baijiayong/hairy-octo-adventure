f3e7193e9d799945a034804dfd89e9f7f95e396b
shihang <study1228.sh@gmail.com>
2014-05-07 12:56:12 +0800

    将test方法抽取到Test类中

---
+++ Test.java
@@ -0,0 +1,19 @@
+class Test
+{
+    public static int success;
+    public static int failed;
+    
+    public static void test(String expected, String result) throws Exception
+    {    
+        if(expected.equals(result))
+        {
+            success++;
+            System.out.println("test success");
+        }
+        else 
+        {
+            failed++;
+            throw new Exception();
+        }       
+    }
+}
